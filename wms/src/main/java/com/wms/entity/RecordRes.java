73fc03e20d9a0a5a5b73b0e521cc5075033f40a2
lihaijian <1595606639@qq.com>
2023-02-16 01:05:29 +0800

    wms

--- wms/src/main/java/com/wms/entity/RecordRes.java
+++ wms/src/main/java/com/wms/entity/RecordRes.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class RecordRes extends  Record{
 
     private String username;
-    private String adminame;
+    private String adminname;
     private String goodsname;
     private String storagename;
     private String goodstypename;
